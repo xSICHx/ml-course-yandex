ff1dd6201075ee417a7b7518a6f5d02f44f88158
xSICHx <nik1mesto@gmail.com>
2025-02-07 18:28:44 +0300

    Выполнена 3 домашка

--- homeworks/assignment03_derivatives/derivatives.py
+++ homeworks/assignment03_derivatives/derivatives.py
@@ -12,11 +12,11 @@ class LossAndDerivatives:
         Return : float
             single number with MSE value of linear model (X.dot(w)) with no bias term
             on the selected dataset.
-        
+
         Comment: If Y is two-dimentional, average the error over both dimentions.
         """
 
-        return np.mean((X.dot(w) - Y)**2)
+        return np.mean((X.dot(w) - Y) ** 2)
 
     @staticmethod
     def mae(X, Y, w):
@@ -24,7 +24,7 @@ class LossAndDerivatives:
         X : numpy array of shape (`n_observations`, `n_features`)
         Y : numpy array of shape (`n_observations`, `target_dimentionality`) or (`n_observations`,)
         w : numpy array of shape (`n_features`, `target_dimentionality`) or (`n_features`,)
-                
+
         Return: float
             single number with MAE value of linear model (X.dot(w)) with no bias term
             on the selected dataset.
@@ -32,8 +32,8 @@ class LossAndDerivatives:
         Comment: If Y is two-dimentional, average the error over both dimentions.
         """
 
-        # YOUR CODE HERE    
-        return 
+        # YOUR CODE HERE
+        return np.mean(np.abs(X.dot(w) - Y))
 
     @staticmethod
     def l2_reg(w):
@@ -45,9 +45,9 @@ class LossAndDerivatives:
 
         Computes the L2 regularization term for the weight matrix w.
         """
-        
+
         # YOUR CODE HERE
-        return 
+        return np.sum(w**2)
 
     @staticmethod
     def l1_reg(w):
@@ -56,38 +56,41 @@ class LossAndDerivatives:
 
         Return : float
             single number with sum of the absolute values of the weight matrix ( \sum_{ij} |w_{ij}| )
-        
+
         Computes the L1 regularization term for the weight matrix w.
         """
 
         # YOUR CODE HERE
-        return 
+        return np.sum(np.abs(w))
 
     @staticmethod
     def no_reg(w):
         """
         Simply ignores the regularization
         """
-        return 0.
-    
+        return 0.0
+
     @staticmethod
     def mse_derivative(X, Y, w):
         """
         X : numpy array of shape (`n_observations`, `n_features`)
         Y : numpy array of shape (`n_observations`, `target_dimentionality`) or (`n_observations`,)
         w : numpy array of shape (`n_features`, `target_dimentionality`) or (`n_features`,)
-        
+
         Return : numpy array of same shape as `w`
 
         Computes the MSE derivative for linear regression (X.dot(w)) with no bias term
         w.r.t. w weight matrix.
-        
+
         Please mention, that in case `target_dimentionality` > 1 the error is averaged along this
         dimension as well, so you need to consider that fact in derivative implementation.
         """
 
         # YOUR CODE HERE
-        return 
+        normalizer = 1
+        if len(Y.shape) > 1:
+            normalizer = Y.shape[1]
+        return 2/X.shape[0]/normalizer *(X.T.dot(X.dot(w) - Y))
 
     @staticmethod
     def mae_derivative(X, Y, w):
@@ -95,18 +98,21 @@ class LossAndDerivatives:
         X : numpy array of shape (`n_observations`, `n_features`)
         Y : numpy array of shape (`n_observations`, `target_dimentionality`) or (`n_observations`,)
         w : numpy array of shape (`n_features`, `target_dimentionality`) or (`n_features`,)
-        
+
         Return : numpy array of same shape as `w`
 
         Computes the MAE derivative for linear regression (X.dot(w)) with no bias term
         w.r.t. w weight matrix.
-        
+
         Please mention, that in case `target_dimentionality` > 1 the error is averaged along this
         dimension as well, so you need to consider that fact in derivative implementation.
         """
-
+        
         # YOUR CODE HERE
-        return 
+        normalizer = 1
+        if len(Y.shape) > 1:
+            normalizer = Y.shape[1]
+        return X.T @ np.sign(X.dot(w) - Y) / X.shape[0] / normalizer
 
     @staticmethod
     def l2_reg_derivative(w):
@@ -119,7 +125,7 @@ class LossAndDerivatives:
         """
 
         # YOUR CODE HERE
-        return 
+        return 2 * w
 
     @staticmethod
     def l1_reg_derivative(w):
@@ -133,7 +139,7 @@ class LossAndDerivatives:
         """
 
         # YOUR CODE HERE
-        return 
+        return np.ones_like(w)
 
     @staticmethod
     def no_reg_derivative(w):
@@ -141,4 +147,3 @@ class LossAndDerivatives:
         Simply ignores the derivative
         """
         return np.zeros_like(w)
-
